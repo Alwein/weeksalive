af9dc1d33b52b8e8c72491ce6eeaca5df16351b9
Alwein <adrien.cens@edu.esiee.fr>
2025-03-31 18:14:17 +0200

    wip

---
+++ scripts/setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Check arguments
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <new_project_name> <new_project_id> <new_app_name>"
+    exit 1
+fi
+
+NEW_PROJECT_NAME=$1
+NEW_PROJECT_ID=$2
+NEW_APP_NAME=$3
+
+OLD_PROJECT_NAME="flutter_fast_template"
+OLD_PROJECT_ID="com.example.flutter_fast_template"
+
+# 1. Update pubspec.yaml
+sed -i '' "s/name: .*/name: $NEW_PROJECT_NAME/" pubspec.yaml
+
+# 2. Update Android app ID and files
+ANDROID_PATH="android/app"
+
+# AndroidManifest.xml
+find "$ANDROID_PATH" -type f -name "AndroidManifest.xml" -exec sed -i '' "s/$OLD_PROJECT_ID/$NEW_PROJECT_ID/g" {} +
+find "$ANDROID_PATH" -type f -name "AndroidManifest.xml" -exec sed -i '' "s/android:label=\".*\"/android:label=\"$NEW_APP_NAME\"/g" {} +
+
+# build.gradle
+sed -i '' "s/applicationId \".*\"/applicationId \"$NEW_PROJECT_ID\"/g" "$ANDROID_PATH/build.gradle"
+sed -i '' "s/namespace = \".*\"/namespace = \"$NEW_PROJECT_ID\"/" "$ANDROID_PATH/build.gradle"
+
+# strings.xml
+sed -i '' "s/<string name=\"app_name\">.*<\/string>/<string name=\"app_name\">$NEW_APP_NAME<\/string>/" "$ANDROID_PATH/src/main/res/values/strings.xml"
+
+# Fix Kotlin package structure
+OLD_ANDROID_PACKAGE_PATH=$(echo "$OLD_PROJECT_ID" | tr '.' '/')
+NEW_ANDROID_PACKAGE_PATH=$(echo "$NEW_PROJECT_ID" | tr '.' '/')
+
+ANDROID_MAIN_PATH="android/app/src/main/kotlin"
+
+mkdir -p "$ANDROID_MAIN_PATH/$NEW_ANDROID_PACKAGE_PATH"
+mv "$ANDROID_MAIN_PATH/$OLD_ANDROID_PACKAGE_PATH/MainActivity.kt" "$ANDROID_MAIN_PATH/$NEW_ANDROID_PACKAGE_PATH/MainActivity.kt"
+sed -i '' "s/package $OLD_PROJECT_ID/package $NEW_PROJECT_ID/" "$ANDROID_MAIN_PATH/$NEW_ANDROID_PACKAGE_PATH/MainActivity.kt"
+rm -rf "$ANDROID_MAIN_PATH/com/example"
+
+# 3. Update iOS app ID
+IOS_PATH="ios"
+
+# Update bundle identifier in project.pbxproj
+find "$IOS_PATH" -name "project.pbxproj" -exec sed -i '' "s/$OLD_PROJECT_ID/$NEW_PROJECT_ID/g" {} +
+find "$IOS_PATH" -name "project.pbxproj" -exec sed -i '' "s/PRODUCT_BUNDLE_IDENTIFIER = .*;/PRODUCT_BUNDLE_IDENTIFIER = $NEW_PROJECT_ID;/g" {} +
+
+# Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleName \"$NEW_APP_NAME\"" "$IOS_PATH/Runner/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName \"$NEW_APP_NAME\"" "$IOS_PATH/Runner/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier \"$NEW_PROJECT_ID\"" "$IOS_PATH/Runner/Info.plist"
+
+
+# 4. Update global project name
+find . -type f -not -path './.git/*' -exec sed -i '' "s/$OLD_PROJECT_NAME/$NEW_PROJECT_NAME/g" {} +
+
+# Rename project directory if needed
+cd ..
+mv "$OLD_PROJECT_NAME" "$NEW_PROJECT_NAME"
+cd "$NEW_PROJECT_NAME"
+
+# 5. Update README.md
+sed -i '' "s/$OLD_PROJECT_NAME/$NEW_PROJECT_NAME/g" README.md
+
+# 6. Clean up git history and reinitialize
+rm -rf .git
+git init
+git add .
+git commit -m "🎉 Initial commit - from template"
+
+# 7. Clean up and get dependencies
+flutter clean
+flutter pub get
+
+# Done
+echo "✅ Project setup completed! 🚀"
