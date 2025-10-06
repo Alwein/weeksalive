056d49f052bbaef22a330a1ae61528cba9f26ffc
Alwein <adrien.cens@edu.esiee.fr>
2025-10-06 09:52:01 +0200

    Add Haptic feedback on navigation events and implement HapticNavigatorObserver in the app

--- lib/presentation/app/app.dart
+++ lib/presentation/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_fast_template/presentation/app/app_bloc_wrapper.dart';
 import 'package:flutter_fast_template/presentation/app/bloc/app_bloc.dart';
@@ -29,7 +30,10 @@ class _AppState extends State<App> {
           localizationsDelegates: context.localizationDelegates,
           supportedLocales: context.supportedLocales,
           locale: context.locale,
-          navigatorObservers: [FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance)],
+          navigatorObservers: [
+            FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
+            HapticNavigatorObserver(),
+          ],
           themeMode: ThemeMode.system,
           builder: (context, child) {
             Jiffy.setLocale(Localizations.localeOf(context).languageCode);
@@ -97,5 +101,19 @@ class _AuthRouterWrapper extends StatelessWidget {
         );
       },
     );
+  }
+}
+
+class HapticNavigatorObserver extends NavigatorObserver {
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
+    HapticFeedback.lightImpact();
+  }
+
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
+    HapticFeedback.lightImpact();
   }
 }
