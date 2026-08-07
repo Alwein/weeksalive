350130cacc87b573135a2cfc1789f0e8b8fdb2ed
Alwein <adrien.cens@edu.esiee.fr>
2026-08-07 16:51:16 +0200

    Add translations for Czech, Danish, German, Greek, Spanish, Finnish, and Indonesian languages

--- lib/main.dart
+++ lib/main.dart
@@ -21,31 +21,27 @@ void main() async {
 
 const supportedLocales = [
   Locale('en', 'US'), // English : en-US.json
-  // Locale('de', 'DE'), // German : de-DE.json
-  // Locale('ar', 'SA'), // Arabic : ar-SA.json
-  // Locale('zh', 'CN'), // Chinese (Simplified) : zh-CN.json
-  // Locale('zh', 'TW'), // Chinese (Traditional) : zh-TW.json
-  // Locale('ko', 'KR'), // Korean : ko-KR.json
-  // Locale('da', 'DK'), // Danish : da-DK.json
-  // Locale('es', 'ES'), // Spanish : es-ES.json
-  // Locale('fi', 'FI'), // Finnish : fi-FI.json
+  Locale('de', 'DE'), // German : de-DE.json
+  Locale('zh', 'CN'), // Chinese (Simplified) : zh-CN.json
+  Locale('ko', 'KR'), // Korean : ko-KR.json
+  Locale('da', 'DK'), // Danish : da-DK.json
+  Locale('es', 'ES'), // Spanish : es-ES.json
+  Locale('fi', 'FI'), // Finnish : fi-FI.json
   Locale('fr', 'FR'), // French : fr-FR.json
-  // Locale('el', 'GR'), // Greek : el-GR.json
-  // Locale('he', 'IL'), // Hebrew : he-IL.json
-  // Locale('hi', 'IN'), // Hindi : hi-IN.json
-  // Locale('id', 'ID'), // Indonesian (Bahasa) : id-ID.json
-  // Locale('it', 'IT'), // Italian : it-IT.json
-  // Locale('ja', 'JP'), // Japanese : ja-JP.json
-  // Locale('nl', 'NL'), // Dutch : nl-NL.json
-  // Locale('no', 'NO'), // Norwegian : no-NO.json
-  // Locale('pl', 'PL'), // Polish : pl-PL.json
-  // Locale('pt', 'BR'), // Portuguese : pt-BR.json
-  // Locale('ro', 'RO'), // Romanian : ro-RO.json
-  // Locale('ru', 'RU'), // Russian : ru-RU.json
-  // Locale('sv', 'SE'), // Swedish : sv-SE.json
-  // Locale('cs', 'CZ'), // Czech : cs-CZ.json
-  // Locale('th', 'TH'), // Thai : th-TH.json
-  // Locale('tr', 'TR'), // Turkish : tr-TR.json
-  // Locale('uk', 'UA'), // Ukrainian : uk-UA.json
-  // Locale('vi', 'VN'), // Vietnamese : vi-VN.json
+  Locale('el', 'GR'), // Greek : el-GR.json
+  Locale('id', 'ID'), // Indonesian (Bahasa) : id-ID.json
+  Locale('it', 'IT'), // Italian : it-IT.json
+  Locale('ja', 'JP'), // Japanese : ja-JP.json
+  Locale('nl', 'NL'), // Dutch : nl-NL.json
+  Locale('no', 'NO'), // Norwegian : no-NO.json
+  Locale('pl', 'PL'), // Polish : pl-PL.json
+  Locale('pt', 'BR'), // Portuguese : pt-BR.json
+  Locale('ro', 'RO'), // Romanian : ro-RO.json
+  Locale('ru', 'RU'), // Russian : ru-RU.json
+  Locale('sv', 'SE'), // Swedish : sv-SE.json
+  Locale('cs', 'CZ'), // Czech : cs-CZ.json
+  Locale('th', 'TH'), // Thai : th-TH.json
+  Locale('tr', 'TR'), // Turkish : tr-TR.json
+  Locale('uk', 'UA'), // Ukrainian : uk-UA.json
+  Locale('vi', 'VN'), // Vietnamese : vi-VN.json
 ];
